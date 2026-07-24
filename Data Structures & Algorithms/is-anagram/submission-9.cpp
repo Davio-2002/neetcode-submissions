3bec6ce2131c0bfec81195e160257ad37e17c8e3
Davio-2002 <45768462+Davio-2002@users.noreply.github.com>
2026-07-24 21:47:18 +0400

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    std::unordered_map<char, int> freq_s{};
+    std::unordered_map<char, int> freq_t{};
+
+    static std::unordered_map<char, int> 
+        check_frequency( std::unordered_map<char, int>& mp, 
+                         const std::string& s)
+    {
+        for(const auto& c : s)
+        {
+            mp[c]++;
+        }         
+
+        return mp;
+    }
+
+    bool isAnagram(string s, string t) {
+        return check_frequency(freq_s, s) == check_frequency(freq_t, t);
+    }
+};
