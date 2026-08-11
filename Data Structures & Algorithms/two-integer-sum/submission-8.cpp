dd95dff23de513a92c544c929b7685d667db5145
Davio-2002 <45768462+Davio-2002@users.noreply.github.com>
2026-08-11 13:42:03 +0400

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> mp{};
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            int complement = target - nums[i];
+            auto it = mp.find(complement);
+            if( it != mp.end())
+            {
+                return {it->second, i };
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
